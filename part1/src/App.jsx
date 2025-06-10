d7aa256e5d19260902c616295e84ae45bacc10b0
Domi <domihg@usal.es>
2025-06-10 14:33:27 +0200

    Changes

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,59 @@
 const App = () => {
-    const course = "Half Stack application development";
-    const part1 = "Fundamentals of React";
-    const exercises1 = 10;
-    const part2 = "Using props to pass data";
-    const exercises2 = 7;
-    const part3 = "State of a component";
-    const exercises3 = 14;
-
-    // return (
-    //     <div>
-    //         <h1>{course}</h1>
-    //         <p>
-    //             {part1} {exercises1}
-    //         </p>
-    //         <p>
-    //             {part2} {exercises2}
-    //         </p>
-    //         <p>
-    //             {part3} {exercises3}
-    //         </p>
-    //         <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    //     </div>
-    // );
+    const course = {
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+            },
+        ],
+    };
 
     return (
         <div>
-            <Header course={course} />
-            <Content />
-            <Total />
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
+    );
+};
+
+const Header = ({ name }) => {
+    return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            <Part name={parts[0].name} exercises={parts[0].exercises} />
+            <Part name={parts[1].name} exercises={parts[1].exercises} />
+            <Part name={parts[2].name} exercises={parts[2].exercises} />
+        </div>
+    );
+};
+
+const Part = ({ name, exercises }) => {
+    return (
+        <p>
+            {name} {exercises}
+        </p>
+    );
+};
+
+const Total = ({parts}) => {
+    return (
+        <p>
+            Number of exercises:{" "}
+            {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        </p>
     );
 };
 
