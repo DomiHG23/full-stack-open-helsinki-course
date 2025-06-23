eac3864233505018f6a4bf8444ee047dcdcd1a69
Domi <domihg@usal.es>
2025-06-23 10:23:49 +0200

    [FEAT] Step 5

---
+++ part2/src/App.jsx
@@ -0,0 +1,98 @@
+const App = () => {
+    const courses = [
+        {
+            name: "Half Stack application development",
+            id: 1,
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: "Redux",
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            name: "Node.js",
+            id: 2,
+            parts: [
+                {
+                    name: "Routing",
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: "Middlewares",
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
+
+    return (
+        <div>
+            {courses.map((course) => (
+                <Course key={course.id} course={course} />
+            ))}
+        </div>
+    );
+};
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    );
+};
+
+const Header = ({ name }) => {
+    return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map((part) => (
+                <Part key={part.id} name={part.name} exercises={part.exercises} />
+            ))}
+        </div>
+    );
+};
+
+const Part = ({ name, exercises }) => {
+    return (
+        <p>
+            {name} {exercises}
+        </p>
+    );
+};
+
+const Total = ({ parts }) => {
+    return (
+        <p style={{ fontWeight: "bold" }}>
+            Number of exercises:{" "}
+            {/* Iterar sobre cada elemento del array (part) almacenando el total en sum, con valor inicial 0 */}
+            {parts.reduce((sum, part) => sum + part.exercises, 0)}
+        </p>
+    );
+};
+
+export default App;
