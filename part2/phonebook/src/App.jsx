e3d3698ffdc1e1ae0a75bdd989cedfd6ff873a7b
Domi <domihg@usal.es>
2025-06-30 10:12:22 +0200

    [FEAT] Exercise 12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -34,18 +34,21 @@ const App = () => {
             id: persons.length + 1,
         };
 
-        setPersons(persons.concat(personObject));
-        setNewName("");
-        setNewNumber("");
+        axios
+            .post("http://localhost:3001/persons", personObject)
+            .then((response) => {
+                console.log(response);
+                setPersons(persons.concat(personObject));
+                setNewName("");
+                setNewNumber("");
+            });
     };
 
     const handleNameChange = (event) => {
-        console.log(event.target.value);
         setNewName(event.target.value);
     };
 
     const handleNumberChange = (event) => {
-        console.log(event.target.value);
         setNewNumber(event.target.value);
     };
 
